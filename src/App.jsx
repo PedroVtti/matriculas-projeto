264852ff3d9cb4b491e04424780b782ff223f627
pedro-vidaletti <pedrovidaletti8@gmail.com>
2024-04-04 22:39:16 -0300

    projeto upado

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [selectedOption, setSelectedOption] = useState(1);
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  const renderTableData = () => {
+    const dataOptions = {
+      1: [
+        ["Turno","Segunda-feira","Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"],
+        ["Manhã", "C. Japonesa", "","","",""],
+        ["Tarde", "", "","C. Japonesa","",""],
+      ],
+      2: [
+        ["Turno","Segunda-feira","Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"],
+        ["Manhã", "", "","Princípios da Culinária","",""],
+        ["Tarde", "Princípios da Culinária", "","","",""],
+      ],
+      3: [
+        ["Turno","Segunda-feira","Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"],
+        ["Manhã", "", "Churrasco I","","",""],
+        ["Tarde", "", "Churrasco I","","",""],
+      ],
+    };
+
+    return dataOptions[selectedOption].map((row, rowIndex) => (
+      <tr key={rowIndex}>
+        {row.map((cell, cellIndex) => (
+          <td key={cellIndex}>{cell}</td>
+        ))}
+      </tr>
+    ));
+  };
+
+  return (
+    <div>
+      <h1>Portal do Professor</h1>
+      <h2>Tabela de Horários:</h2>
+      <button onClick={() => handleOptionChange(1)}>Prof. Aldo</button>
+      <button onClick={() => handleOptionChange(2)}>Prof. Bento</button>
+      <button onClick={() => handleOptionChange(3)}>Prof. Claudio</button>
+
+      <table border="1">
+        <tbody>{renderTableData()}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [selectedOption, setSelectedOption] = useState(1);
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  const renderTableData = () => {
+    const dataOptions = {
+      1: [
+        ["Turno","Segunda-feira","Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"],
+        ["Manhã", "C. Japonesa", "Princípios da Culinária","","",""],
+        ["Tarde", "", "","","",""],
+      ],
+      2: [
+        ["Turno","Segunda-feira","Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"],
+        ["Manhã", "C. Japonesa", "Churrasco I","Princípios da Culinária","",""],
+        ["Tarde", "Princípios da Culinária", "","","",""],
+      ],
+      3: [
+        ["Turno","Segunda-feira","Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"],
+        ["Manhã", "C. Japonesa", "Churrasco I","Princípios da Culinária","",""],
+        ["Tarde", "Princípios da Culinária", "Churrasco I","C. Japonesa","",""],
+      ],
+    };
+
+    return dataOptions[selectedOption].map((row, rowIndex) => (
+      <tr key={rowIndex}>
+        {row.map((cell, cellIndex) => (
+          <td key={cellIndex}>{cell}</td>
+        ))}
+      </tr>
+    ));
+  };
+
+  return (
+    <div>
+      <h1>Portal do Aluno</h1>
+      <h2>Tabela de Horários:</h2>
+      <h3>Curso de Gastronomia intensivo</h3>
+      <P>Escolha a sua opção de matrícula:</P>
+      <button onClick={() => handleOptionChange(1)}>2 disciplinas</button>
+      <button onClick={() => handleOptionChange(2)}>4 disciplinas</button>
+      <button onClick={() => handleOptionChange(3)}>6 disciplinas</button>
+
+      <table border="1">
+        <tbody>{renderTableData()}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
