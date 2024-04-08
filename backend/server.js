dac7b1790533830c21c427854f98bf9187ac3651
Pedro Pelizzaro <pedro.pelizzaro@gmail.com>
2024-04-08 11:40:41 -0300

    limpando comentários

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,16 @@ const port = 3000
 var path = require ("path");
 var fs = require("fs");
 app.get('/', (req, res) => {
-  // let indexPath = path.resolve('../src/main.jsx');
-  let indexPath = path.join(__dirname, "../index.html");
-  console.log(indexPath);
-  res.sendFile(indexPath);
+  res.send("Nada nesse endpoint. Tente /turnos!")
 })
 
 app.get('/turnos', function (req, res) {
-    console.log("Turnos requested");
+    console.log("Turnos foram pedidos");
     res.header("Access-Control-Allow-Origin", "*");
-    let filePath = __dirname + '/db/turnosProfessores.json';
-    // let turnosJSON = JSON.parse(filePath);
-    // console.log(turnosJSON);
 
     fs.readFile(__dirname + "/db/turnosProfessores.json", function (err, data) {
-      console.log(" data");
-      console.log(data)  
       let turnosJSON = JSON.parse(data);
-        
-      console.log("Parsed")
-        console.log(turnosJSON)
+      console.log(turnosJSON)
        res.send(turnosJSON);
     });
     
