2aeff0b02bde920b0d8d70483f40d3c394d1f0f8
Pedro Pelizzaro <pedro.pelizzaro@gmail.com>
2024-04-07 23:45:37 -0300

    corrigindo import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppProfessor from './AppProfessor.jsx'
-import './AppAluno.css'
+import './AppProfessor.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
